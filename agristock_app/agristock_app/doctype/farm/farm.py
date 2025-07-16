076b005815543d55ad7575d626e7441847d5c233
Rahul <rahul.singh@nestorbird.com>
2025-07-16 10:15:08 +0530

    Delete agristock_app/agristock_app/doctype/farm directory

--- agristock_app/agristock_app/doctype/farm/farm.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Rahul Bisht and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Farm(Document):
-	pass
